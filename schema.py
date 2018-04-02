2f31b98d7b55af24c71d29668a356a9efbaa728c
D.A.Bashkirtsevich <bashkirtsevich@gmail.com>
2018-04-02 21:55:00 +0500

    Schema basics

---
+++ schema.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, MetaData, Table, Text, ForeignKey, UniqueConstraint, DateTime
+
+metadata = MetaData()
+
+entity_type = Table(
+    "entity_type", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", nullable=False, index=True, unique=True),
+)
+
+entity = Table(
+    "entity", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("type", ForeignKey("entity_type.id"), index=True, nullable=False),
+    Column("token", nullable=False, index=True),
+    UniqueConstraint("type", "token"),
+)
+
+user = Table(
+    "user", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("token", nullable=False, index=True, unique=True),
+)
+
+comment = Table(
+    "comment", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("entity", ForeignKey("entity.id"), index=True, nullable=False),
+    Column("user", ForeignKey("user.id"), index=True, nullable=False),
+    Column("user", ForeignKey("comment.id"), index=True),
+)
+
+comment_data = Table(
+    "comment_data", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("comment", ForeignKey("comment.id"), index=True, nullable=False),
+    Column("timestamp", DateTime, index=True, nullable=False),
+    Column("hash", Text, index=True),
+    Column("data", Text, index=True),
+    UniqueConstraint("id", "hash"),
+)
