f2bd8fd11c7eb18d4250db482f2025a9d2e3f841
D.A.Bashkirtsevich <bashkirtsevich@gmail.com>
2018-04-03 13:09:18 +0500

    Add remove_comment

--- api.py
+++ api.py
@@ -22,13 +22,18 @@ async def add_comment(connection, entity_type, entity_token, user_token, text):
     return result
 
 
-async def edit_comment(connection, user_token, comment_unique_key, text):
+async def _try_get_comment(connection, user_token, comment_unique_key):
     user_id = await db_api.get_user_id_by_token(connection, user_token)
     comment = await db_api.get_comment_by_key(connection, comment_unique_key)
 
     if comment["user"] != user_id:
         raise Exception("Access denied. Invalid user token.")
+    else:
+        return comment
 
+
+async def edit_comment(connection, user_token, comment_unique_key, text):
+    comment = await _try_get_comment(connection, user_token, comment_unique_key)
     result = await db_api.add_or_update_comment_text(
         connection,
         comment_id=comment["id"],
@@ -36,4 +41,10 @@ async def edit_comment(connection, user_token, comment_unique_key, text):
         text_hash=sha1(text)
     )
 
+    return result
+
+
+async def remove_comment(connection, user_token, comment_unique_key):
+    comment = await _try_get_comment(connection, user_token, comment_unique_key)
+    result = await db_api.delete_comment(connection, comment["id"])
     return result
