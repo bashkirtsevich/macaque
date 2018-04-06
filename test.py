3fb69a96d075fc5f2f679c37b9b7be3c2083a566
D.A.Bashkirtsevich <bashkirtsevich@gmail.com>
2018-04-06 15:30:54 +0500

    assert -> self.assert

--- test.py
+++ test.py
@@ -19,13 +19,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "text": "Test message"
             }
         )
-        assert resp.status == 200
+        self.assertTrue(resp.status == 200)
 
         result = await resp.json()
-        assert "result" in result
-        assert isinstance(result["result"], dict)
-        assert "comment_token" in result["result"]
-        assert isinstance(result["result"]["comment_token"], str)
+        self.assertTrue("result" in result)
+        self.assertTrue(isinstance(result["result"], dict))
+        self.assertTrue("comment_token" in result["result"])
+        self.assertTrue(isinstance(result["result"]["comment_token"], str))
 
     @unittest_run_loop
     async def test_add_reply(self):
@@ -37,13 +37,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "text": "Hi, nice video!"
             }
         )
-        assert resp1.status == 200
+        self.assertTrue(resp1.status == 200)
 
         resp1_result = await resp1.json()
-        assert "result" in resp1_result
-        assert isinstance(resp1_result["result"], dict)
-        assert "comment_token" in resp1_result["result"]
-        assert isinstance(resp1_result["result"]["comment_token"], str)
+        self.assertTrue("result" in resp1_result)
+        self.assertTrue(isinstance(resp1_result["result"], dict))
+        self.assertTrue("comment_token" in resp1_result["result"])
+        self.assertTrue(isinstance(resp1_result["result"]["comment_token"], str))
 
         # Add reply for comment
         resp2 = await self.client.post(
@@ -53,13 +53,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "text": "Lol, WTF? Not so interesting!"
             }
         )
-        assert resp2.status == 200
+        self.assertTrue(resp2.status == 200)
 
         resp2_result = await resp2.json()
-        assert "result" in resp2_result
-        assert isinstance(resp2_result["result"], dict)
-        assert "comment_token" in resp2_result["result"]
-        assert isinstance(resp2_result["result"]["comment_token"], str)
+        self.assertTrue("result" in resp2_result)
+        self.assertTrue(isinstance(resp2_result["result"], dict))
+        self.assertTrue("comment_token" in resp2_result["result"])
+        self.assertTrue(isinstance(resp2_result["result"]["comment_token"], str))
 
     @unittest_run_loop
     async def test_edit_comment(self):
@@ -71,13 +71,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "text": "Cooooooooool video!"
             }
         )
-        assert resp1.status == 200
+        self.assertTrue(resp1.status == 200)
 
         resp1_result = await resp1.json()
-        assert "result" in resp1_result
-        assert isinstance(resp1_result["result"], dict)
-        assert "comment_token" in resp1_result["result"]
-        assert isinstance(resp1_result["result"]["comment_token"], str)
+        self.assertTrue("result" in resp1_result)
+        self.assertTrue(isinstance(resp1_result["result"], dict))
+        self.assertTrue("comment_token" in resp1_result["result"])
+        self.assertTrue(isinstance(resp1_result["result"]["comment_token"], str))
 
         # Edit comment
         resp2 = await self.client.post(
@@ -86,13 +86,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "text": "Sorry, I don't think so :("
             }
         )
-        assert resp2.status == 200
+        self.assertTrue(resp2.status == 200)
 
         resp2_result = await resp2.json()
-        assert "result" in resp2_result
-        assert isinstance(resp2_result["result"], bool)
-        assert "success" in resp2_result["result"]
-        assert resp2_result["result"]["success"]
+        self.assertTrue("result" in resp2_result)
+        self.assertTrue(isinstance(resp2_result["result"], bool))
+        self.assertTrue("success" in resp2_result["result"])
+        self.assertTrue(resp2_result["result"]["success"])
 
         # Edit message with same text value
         resp3 = await self.client.post(
@@ -101,13 +101,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "text": "Sorry, I don't think so :("
             }
         )
-        assert resp3.status == 200
+        self.assertTrue(resp3.status == 200)
 
         resp3_result = await resp3.json()
-        assert "result" in resp3_result
-        assert isinstance(resp3_result["result"], bool)
-        assert "success" in resp3_result["result"]
-        assert not resp3_result["result"]["success"]
+        self.assertTrue("result" in resp3_result)
+        self.assertTrue(isinstance(resp3_result["result"], bool))
+        self.assertTrue("success" in resp3_result["result"])
+        self.assertTrue(not resp3_result["result"]["success"])
 
     @unittest_run_loop
     async def test_remove_comment(self):
@@ -119,13 +119,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "text": "I must delete this message..."
             }
         )
-        assert resp1.status == 200
+        self.assertTrue(resp1.status == 200)
 
         resp1_result = await resp1.json()
-        assert "result" in resp1_result
-        assert isinstance(resp1_result["result"], dict)
-        assert "comment_token" in resp1_result["result"]
-        assert isinstance(resp1_result["result"]["comment_token"], str)
+        self.assertTrue("result" in resp1_result)
+        self.assertTrue(isinstance(resp1_result["result"], dict))
+        self.assertTrue("comment_token" in resp1_result["result"])
+        self.assertTrue(isinstance(resp1_result["result"]["comment_token"], str))
 
         # Delete comment
         resp2 = await self.client.post(
@@ -134,13 +134,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "user_token": "test_remove_comment"
             }
         )
-        assert resp2.status == 200
+        self.assertTrue(resp2.status == 200)
 
         resp2_result = await resp2.json()
-        assert "result" in resp2_result
-        assert isinstance(resp2_result["result"], bool)
-        assert "success" in resp2_result["result"]
-        assert resp2_result["result"]["success"]
+        self.assertTrue("result" in resp2_result)
+        self.assertTrue(isinstance(resp2_result["result"], bool))
+        self.assertTrue("success" in resp2_result["result"])
+        self.assertTrue(resp2_result["result"]["success"])
 
     @unittest_run_loop
     async def test_remove_comment2(self):
@@ -152,13 +152,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "text": "I must delete this message..."
             }
         )
-        assert resp1.status == 200
+        self.assertTrue(resp1.status == 200)
 
         resp1_result = await resp1.json()
-        assert "result" in resp1_result
-        assert isinstance(resp1_result["result"], dict)
-        assert "comment_token" in resp1_result["result"]
-        assert isinstance(resp1_result["result"]["comment_token"], str)
+        self.assertTrue("result" in resp1_result)
+        self.assertTrue(isinstance(resp1_result["result"], dict))
+        self.assertTrue("comment_token" in resp1_result["result"])
+        self.assertTrue(isinstance(resp1_result["result"]["comment_token"], str))
 
         # Add reply for comment
         resp2 = await self.client.post(
@@ -168,13 +168,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "text": "Lol, WTF? Not so interesting!"
             }
         )
-        assert resp2.status == 200
+        self.assertTrue(resp2.status == 200)
 
         resp2_result = await resp2.json()
-        assert "result" in resp2_result
-        assert isinstance(resp2_result["result"], dict)
-        assert "comment_token" in resp2_result["result"]
-        assert isinstance(resp2_result["result"]["comment_token"], str)
+        self.assertTrue("result" in resp2_result)
+        self.assertTrue(isinstance(resp2_result["result"], dict))
+        self.assertTrue("comment_token" in resp2_result["result"])
+        self.assertTrue(isinstance(resp2_result["result"]["comment_token"], str))
 
         # Delete comment
         resp3 = await self.client.post(
@@ -183,13 +183,13 @@ class ApplicationTestCase(AioHTTPTestCase):
                 "user_token": "test_remove_comment2"
             }
         )
-        assert resp3.status == 200
+        self.assertTrue(resp3.status == 200)
 
         resp3_result = await resp3.json()
-        assert "result" in resp3_result
-        assert isinstance(resp3_result["result"], bool)
-        assert "success" in resp3_result["result"]
-        assert not resp3_result["result"]["success"]
+        self.assertTrue("result" in resp3_result)
+        self.assertTrue(isinstance(resp3_result["result"], bool))
+        self.assertTrue("success" in resp3_result["result"])
+        self.assertFalse(resp3_result["result"]["success"])
 
     @unittest_run_loop
     async def test_get_comments(self):
@@ -201,70 +201,70 @@ class ApplicationTestCase(AioHTTPTestCase):
                     "text": "Short message #{} for test_get_comments example".format(idx)
                 }
             )
-            assert resp.status == 200
+            self.assertTrue(resp.status == 200)
 
             result = await resp.json()
-            assert "result" in result
-            assert isinstance(result["result"], dict)
-            assert "comment_token" in result["result"]
-            assert isinstance(result["result"]["comment_token"], str)
+            self.assertTrue("result" in result)
+            self.assertTrue(isinstance(result["result"], dict))
+            self.assertTrue("comment_token" in result["result"])
+            self.assertTrue(isinstance(result["result"]["comment_token"], str))
             return result["result"]["comment_token"]
 
         comment_tokens = [await insert_comment(i) for i in range(1000)]
 
         # Read1
         resp1 = await self.client.get("/api/comments/type2/entity1")
-        assert resp1.status == 200
+        self.assertTrue(resp1.status == 200)
 
         resp1_result = await resp1.json()
-        assert "result" in resp1_result
-        assert isinstance(resp1_result["result"], list)
-        assert len(resp1_result["result"]) == 1000
+        self.assertTrue("result" in resp1_result)
+        self.assertTrue(isinstance(resp1_result["result"], list))
+        self.assertTrue(len(resp1_result["result"]) == 1000)
         for item in resp1_result["result"]:
-            assert isinstance(item, dict)
-            assert "text" in item
-            assert "created" in item
-            assert "updated" in item
-            assert "user" in item
-            assert "key" in item
-            assert "parent_key" in item
-            assert item["key"] in comment_tokens
+            self.assertTrue(isinstance(item, dict))
+            self.assertTrue("text" in item)
+            self.assertTrue("created" in item)
+            self.assertTrue("updated" in item)
+            self.assertTrue("user" in item)
+            self.assertTrue("key" in item)
+            self.assertTrue("parent_key" in item)
+            self.assertTrue(item["key"] in comment_tokens)
 
         # Read2
         resp1 = await self.client.get("/api/comments/type2/entity1/100")
-        assert resp1.status == 200
+        self.assertTrue(resp1.status == 200)
 
         resp1_result = await resp1.json()
-        assert "result" in resp1_result
-        assert isinstance(resp1_result["result"], list)
-        assert len(resp1_result["result"]) == 100
+        self.assertTrue("result" in resp1_result)
+        self.assertTrue(isinstance(resp1_result["result"], list))
+        self.assertTrue(len(resp1_result["result"]) == 100)
         for item in resp1_result["result"]:
-            assert isinstance(item, dict)
-            assert "text" in item
-            assert "created" in item
-            assert "updated" in item
-            assert "user" in item
-            assert "key" in item
-            assert "parent_key" in item
-            assert item["key"] in comment_tokens
+            self.assertTrue(isinstance(item, dict))
+            self.assertTrue("text" in item)
+            self.assertTrue("created" in item)
+            self.assertTrue("updated" in item)
+            self.assertTrue("user" in item)
+            self.assertTrue("key" in item)
+            self.assertTrue("parent_key" in item)
+            self.assertTrue(item["key"] in comment_tokens)
 
         # Read3
         resp1 = await self.client.get("/api/comments/type2/entity1/100/100")
-        assert resp1.status == 200
+        self.assertTrue(resp1.status == 200)
 
         resp1_result = await resp1.json()
-        assert "result" in resp1_result
-        assert isinstance(resp1_result["result"], list)
-        assert len(resp1_result["result"]) == 100
+        self.assertTrue("result" in resp1_result)
+        self.assertTrue(isinstance(resp1_result["result"], list))
+        self.assertTrue(len(resp1_result["result"]) == 100)
         for item in resp1_result["result"]:
-            assert isinstance(item, dict)
-            assert "text" in item
-            assert "created" in item
-            assert "updated" in item
-            assert "user" in item
-            assert "key" in item
-            assert "parent_key" in item
-            assert item["key"] in comment_tokens
+            self.assertTrue(isinstance(item, dict))
+            self.assertTrue("text" in item)
+            self.assertTrue("created" in item)
+            self.assertTrue("updated" in item)
+            self.assertTrue("user" in item)
+            self.assertTrue("key" in item)
+            self.assertTrue("parent_key" in item)
+            self.assertTrue(item["key"] in comment_tokens)
 
 
 if __name__ == '__main__':
